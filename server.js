87bc0ab5bb7073be22cf7b297343335bfdaa3f57
benne <ben.caulfield@outlook.com>
2020-09-23 10:13:00 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-
+const app = require('express')();
+const basicAuth = require('express-basic-auth');
+app.set('view engine', 'ejs')
+ 
+app.use(basicAuth({
+    users: { admin: 'supersecret123' }, //change later
+    challenge: true // shows login 
+}));
